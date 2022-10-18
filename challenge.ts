27d7ce877f5c933bef912a5dc29587d9d2c6d768
Valeria Aizcorbe <valeria.aizcorbe@hotmail.com>
2022-10-17 22:21:02 -0600

    Challenge completed

---
+++ challenge.ts
@@ -0,0 +1,18 @@
+// Create a User class
+//Add a firstName, lastName and email
+//Add a get to return fullName
+//Add a method to check if your emal param matches the user's current email
+
+export class User {
+    firstName: string;
+    lastName: string;
+    email: string;
+
+    get fullName(): string {
+        return `${this.firstName} ${this.lastName}`
+    }
+
+    checkMatch(email: string): boolean{
+        return this.email ===email
+    }
+}
